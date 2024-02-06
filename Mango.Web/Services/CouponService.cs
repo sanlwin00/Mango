11dbe976a02c4222af3aa5b8914e365d723b1250
San Oo <san.oo@pricedex.com>
2024-02-05 19:38:45 -0500

    Initial commit

---
+++ Mango.Web/Services/CouponService.cs
@@ -0,0 +1,28 @@
+﻿
+using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Services;
+
+public class BaseService : IBaseService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    public BaseService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public Task<ResponseDto?> SendAsync(RequestDto requestDto)
+    {
+        HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+        HttpRequestMessage message = new();
+        message.Headers.Add("Accept", "application/json");
+        // token
+        message.RequestUri = new Uri(requestDto.Url);
+        if (requestDto.Data != null)
+        {
+            message.Content = new StringContent(JsonConvert.SerializeObject)
+        }
+        throw new NotImplementedException();
+    }
+}
---
+++ Mango.Web/Utility/StaticData.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Web;
+
+public class StaticData
+{
+    public enum ApiType
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+}
