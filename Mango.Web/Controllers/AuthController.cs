59f3fa8306eaef71de517cb5348b0ca0a82ed859
San <kosanlwinoo@gmail.com>
2024-02-08 20:46:06 -0500

    Fixed AuthService Url

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using Mango.Web.Models.Dto;
+using Mango.Web.Models;
 using Mango.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Runtime.InteropServices;
 
 namespace Mango.Web.Controllers
@@ -15,14 +17,35 @@ namespace Mango.Web.Controllers
         [HttpGet]
         public IActionResult Login()
         {
-            LoginRequestDto loginRequestDto = new();
-            return View(loginRequestDto);
+            LoginRequestDto obj = new();
+            return View(obj);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto? obj)
+        {
+            try{
+                ResponseDto? response = await _authService.LoginAsync(obj);
+                if (response != null && response.IsSuccess)
+                {
+                    LoginResponseDto loginResponse = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(response.Result));
+                    RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    TempData["error"] = response.Message;
+                }
+            }
+            catch(Exception ex)
+            {
+                TempData["error"] = ex.Message;
+            }
+            return View(obj);
         }
         [HttpGet]
         public IActionResult Register()
         {
-            //RegistrationDto registrationDto = new();
-            return View();
+            RegistrationDto registrationDto = new();
+            return View(registrationDto);
         }
         [HttpGet]
         public IActionResult Logout()
