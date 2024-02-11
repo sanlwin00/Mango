e9d0e0e4d5705e77ba8d20291b7bffa9068fd3fc
sanlwin00 <kosanlwinoo@gmail.com>
2024-02-10 20:04:31 -0500

    Added product list display to Home Page and created Product Details page

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,21 +1,55 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Mango.Web.Models;
+using Newtonsoft.Json;
+using Mango.Web.Services.IServices;
+using Mango.Web.Services;
 
 namespace Mango.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IProductService _productService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IProductService productService)
     {
         _logger = logger;
+        _productService = productService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        List<ProductDto>? list = new();
+
+        ResponseDto? response = await _productService.GetAllProductAsync();
+        if (response != null && response.IsSuccess)
+        {
+            list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+        }
+        else
+        {
+            TempData["error"] = response?.Message;
+        }
+        return View(list);
+
+    }
+
+    public async Task<IActionResult> ProductDetails(int productId)
+    {
+        ProductDto? dto = new();
+
+        ResponseDto? response = await _productService.GetProductbyIdAsync(productId);
+        if (response != null && response.IsSuccess)
+        {
+            dto = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+        }
+        else
+        {
+            TempData["error"] = response?.Message;
+        }
+        return View(dto);
+
     }
 
     public IActionResult Privacy()
