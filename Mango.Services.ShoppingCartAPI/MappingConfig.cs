79415436e2bf48c51a8ba8207f72a46c0ea2d0f9
sanlwin00 <kosanlwinoo@gmail.com>
2024-02-10 20:54:01 -0500

    Added ShoppingCartAPI and DB table setup done

---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeaderDto, CartHeader>();
+                config.CreateMap<CartHeader,  CartHeaderDto>();
+                config.CreateMap<CartDetailDto, CartDetail>();
+                config.CreateMap<CartDetail, CartDetailDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
+
+
---
+++ Mango.Services.ShoppingCartAPI/Program.cs
@@ -0,0 +1,56 @@
+using AutoMapper;
+using Mango.Services.ShoppingCartAPI;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//* Add DBContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//* Add Automapper
+IMapper mapper = MappingConfig.RegisterMappings().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+//* Add Authentication, Authorization and Swagger
+builder.AddJwtAuthenticationAndSwagger(); //custom extension
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();    //* use jwt auth
+app.UseAuthorization();
+
+app.MapControllers();
+ApplyMigration();           //* apply changes to db
+app.Run();
+
+//* Auto apply any pending migration
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
