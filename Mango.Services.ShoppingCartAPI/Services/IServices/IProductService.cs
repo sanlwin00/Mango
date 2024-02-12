f0358082bf3c9c7215041f52c7e6db686de8e3d8
sanlwin00 <kosanlwinoo@gmail.com>
2024-02-11 21:31:56 -0500

    Added GetCart method and ProductService to CarAPI

---
+++ Mango.Services.ShoppingCartAPI/Services/IServices/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<ResponseDto> GetProductbyIdAsync(int id);
+    }
+}
