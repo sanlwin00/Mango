375ad24db555b378f39e49f71ed06992c8c53825
sanlwin00 <kosanlwinoo@gmail.com>
2024-02-24 12:21:11 -0500

    Added Email Message Queue support for Registered Users

--- Mango.Services.EmailAPI/Services/EmailService.cs
+++ Mango.Services.EmailAPI/Services/EmailService.cs
@@ -16,7 +16,7 @@ namespace Mango.Services.EmailAPI.Services
             this._dbOptions = options;
         }
 
-        public async Task<bool> SendEmailAndLog(CartDto cartDto)
+        public async Task<bool> SendCartEmail(CartDto cartDto)
         {
             var message = GetFormattedMessage(cartDto);
             EmailLog emailLog = new()
@@ -41,11 +41,36 @@ namespace Mango.Services.EmailAPI.Services
             }            
         }
 
+        public async Task<bool> SendRegistrationEmail(RegistrationDto registerDto)
+        {
+            var message = GetFormattedMessage(registerDto);
+            EmailLog emailLog = new()
+            {
+                EmailAddress = registerDto.Email,
+                SentOn = DateTime.Now,
+                Message = message
+            };
+
+            try
+            {
+                await using var _db = new AppDbContext(_dbOptions);
+                _db.Emails.Add(emailLog);
+                await _db.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
         private string GetFormattedMessage(CartDto cartDto)
         {
             var message = new StringBuilder();
             message.AppendLine("<br/>Your Cart");
-            message.AppendLine($"<br/>Total: {cartDto.CartHeader.CartTotal}");
+            message.AppendLine($"<br/>Total: {cartDto.CartHeader.CartTotal.ToString("C")}");
             message.AppendLine("<p><ul>");
             foreach(var item in cartDto.CartDetails)
             {
@@ -54,6 +79,14 @@ namespace Mango.Services.EmailAPI.Services
                 message.AppendLine("</li>");
             }
             message.AppendLine("</ul></p>");
+            return message.ToString();
+        }
+
+        private string GetFormattedMessage(RegistrationDto registerDto)
+        {
+            var message = new StringBuilder();
+            message.AppendLine($"<h1>Welcome {registerDto.Name}</h1>");            
+            message.AppendLine("<p>Thank you for registering</p>");
             return message.ToString();
         }
     }
